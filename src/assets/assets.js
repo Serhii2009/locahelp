232344ae2ecea433a62c778d4ad279c52ea88b56
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-08-06 12:49:15 +0300

    created a NavBar part

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,11 @@
 import menu_bar from './menu_bar.svg'
 import moon from './moon.svg'
 import logo from './logo.svg'
+import menu_bar_close from './menu_bar_close.svg'
 
 export const assets = {
   menu_bar,
   moon,
   logo,
+  menu_bar_close,
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,14 @@
 import './NavBar.css'
 import { assets } from '../../assets/assets.js'
+import { useState } from 'react'
 
 const NavBar = () => {
+  const [modal, setModal] = useState(false)
+
+  const toogleModal = () => {
+    setModal(!modal)
+  }
+
   return (
     <div className="navbar">
       <img className="navbar-logo" src={assets.logo} alt="" />
@@ -16,7 +23,33 @@ const NavBar = () => {
 
       <div className="navbar-right">
         <div className="navbar-signin">Sign In</div>
-        <img src={assets.menu_bar} alt="" />
+        <img
+          onClick={toogleModal}
+          className="nav-mob-open"
+          src={assets.menu_bar}
+          alt=""
+        />
+
+        {modal && (
+          <div className="modal">
+            <div className="modal-component">
+              <div className="modal-list">
+                <li>About Us</li>
+                <li className="modal-li">Our Mission</li>
+                <li className="modal-li">Our Policy</li>
+                <li className="modal-li">Support </li>
+                <li className="modal-li">Relaxing game</li>
+              </div>
+
+              <img
+                onClick={toogleModal}
+                className="nav-mob-close"
+                src={assets.menu_bar_close}
+                alt=""
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
