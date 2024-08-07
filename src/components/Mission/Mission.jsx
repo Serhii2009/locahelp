bff98027ec25e8b9a70d1055a0d7e44657297bee
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-08-07 13:46:23 +0300

    created our mission section

---
+++ src/components/Mission/Mission.jsx
@@ -0,0 +1,29 @@
+import './Mission.css'
+
+const Mission = () => {
+  return (
+    <div className="mission">
+      <div className="mission-top">
+        <h1>Locahelp’s Mission</h1>
+        <p>
+          To empower individuals and families to confidently embrace new
+          horizons by providing unparalleled relocation services. We strive to
+          simplify the complexities of international moves, offering expert
+          guidance and support every step of the way. With a deep understanding
+          of cultural nuances and global mobility, we are committed to creating
+          seamless and enriching relocation experiences.
+        </p>
+      </div>
+
+      <div className="mission-down">
+        <h1>Our Vision</h1>
+        <p>
+          To be the leading global relocation partner, trusted for our
+          expertise, compassion, and dedication to client satisfaction.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Mission
--- src/pages/OurMission/OurMission.jsx
+++ src/pages/OurMission/OurMission.jsx
@@ -1,10 +1,58 @@
 import './OurMission.css'
-import Background from '../../components/Background/Background'
+import { useEffect, useState } from 'react'
+import NavBar from '../../components/NavBar/NavBar'
+import { assets } from '../../assets/assets'
+import Mission from '../../components/Mission/Mission'
 
 const OurMission = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme === 'dark') {
+      setIsDarkTheme(true)
+      document.documentElement.classList.add('dark-theme')
+    }
+  }, [])
+
+  const toggleTheme = () => {
+    const newTheme = !isDarkTheme
+    setIsDarkTheme(newTheme)
+    if (newTheme) {
+      document.documentElement.classList.add('dark-theme')
+      localStorage.setItem('theme', 'dark')
+    } else {
+      document.documentElement.classList.remove('dark-theme')
+      localStorage.setItem('theme', 'light')
+    }
+  }
+
   return (
-    <div>
-      <Background />
+    <div className="generalStaticBack-mission">
+      <div className="background-box-mission">
+        <NavBar />
+        <Mission />
+        <div className="background-static-img-mission">
+          <img
+            src={assets.moon}
+            alt="moon"
+            onClick={toggleTheme}
+            style={{
+              cursor: 'pointer',
+              display: isDarkTheme ? 'none' : 'block',
+            }}
+          />
+          <img
+            src={assets.sun}
+            alt="sun"
+            onClick={toggleTheme}
+            style={{
+              cursor: 'pointer',
+              display: isDarkTheme ? 'block' : 'none',
+            }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
